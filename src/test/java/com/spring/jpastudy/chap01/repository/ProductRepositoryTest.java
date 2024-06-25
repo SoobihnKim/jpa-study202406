0fe351a0f1cfdf2dda10c4570c787f56d4761e11
SoobihnKim <ksbsj92@naver.com>
2024-06-25 11:24:19 +0900

    # 3. 컬럼 기본값 설정, @BeforeEach 데이터, 삭제 테스트

--- src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
@@ -1,29 +1,62 @@
 package com.spring.jpastudy.chap01.repository;
 
 import com.spring.jpastudy.chap01.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.spring.jpastudy.chap01.entity.Product.Category.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback
 class ProductRepositoryTest {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @BeforeEach
+    void insertBeforeTest() {
+        Product p1 = Product.builder()
+                .name("아이폰")
+                .category(ELECTRONIC)
+                .price(2000000)
+                .build();
+        Product p2 = Product.builder()
+                .name("탕수육")
+                .category(FOOD)
+                .price(20000)
+                .build();
+        Product p3 = Product.builder()
+                .name("구두")
+                .category(FASHION)
+                .price(300000)
+                .build();
+        Product p4 = Product.builder()
+                .name("주먹밥")
+                .category(FOOD)
+                .price(1500)
+                .build();
+
+        productRepository.save(p1);
+        productRepository.save(p2);
+        productRepository.save(p3);
+        productRepository.save(p4);
+    }
 
     @Test
     @DisplayName("상품을 데이터베이스에 저장한다.")
     void saveTest() {
         //given
         Product product = Product.builder()
-                .name("귀걸이")
-                .price(140000)
-                .category(Product.Category.FASHION)
+                .name("떡볶이")
+//                .price(140000)
+//                .category(Product.Category.FASHION)
                 .build();
 
         //when
@@ -31,7 +64,19 @@ class ProductRepositoryTest {
         Product saved = productRepository.save(product);
         //then
         assertNotNull(saved);
-
     }
+
+    @Test
+    @DisplayName("1번 상품을 삭제한다")
+    void deleteTest() {
+        //given
+        Long id = 1L;
+        //when
+        productRepository.deleteById(id);
+        //then
+        Product foundProduct = productRepository.findById(id).orElse(null);
+        assertNull(foundProduct);
+    }
+
 
 }
