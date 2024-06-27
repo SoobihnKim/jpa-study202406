9f6f8dc8ad1a6c481b64180dec630ae99110351b
SoobihnKim <ksbsj92@naver.com>
2024-06-27 11:58:57 +0900

    # QueryDSL과 JPQL의 차이점

--- src/main/java/com/spring/jpastudy/config/QueryDslConfig.java
+++ src/main/java/com/spring/jpastudy/config/QueryDslConfig.java
@@ -16,6 +16,7 @@ public class QueryDslConfig {
     @PersistenceContext
     private EntityManager em;
 
+    // "<bean id='jpaQueryFactory' class='com.querydsl.jpa.impl.JPAQueryFactory' />"
     @Bean // 외부 라이브러리를 스프링 컨테이너에 관리시키는 설정
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em);
